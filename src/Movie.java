0b860d2c9a3f45a5e4a4a398eecf657d784f1e48
TvRXVII <TvRXVII@users.noreply.github.com>
2019-05-07 15:43:18 +0200

    Used new Classes

--- src/Movie.java
+++ src/Movie.java
@@ -5,7 +5,7 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     
     private String title;
-    private int priceCode;
+    private Price _priceCode;
     
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
@@ -13,36 +13,29 @@ public class Movie {
     }
     
     public int getPriceCode() {
-        return priceCode;
+        return _priceCode.getPriceCode();
     }
     
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch(arg) {
+        case REGULAR:
+        	_priceCode = new RegularPrice();
+        	break;
+        case CHILDRENS:
+        	_priceCode = new ChildrensPrice();
+        	break;
+        case NEW_RELEASE:
+        	_priceCode = new NewReleasePrice();
+        	break;
+        default:
+        	throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
     
     public String getTitle (){
         return title;
     };
     
-	double getCharge(int daysRented) {
-	    double result = 0;
-	    switch (getPriceCode()) {
-	        case Movie.REGULAR:
-	            result += 2;
-	            if (daysRented > 2)
-	                result += (daysRented - 2) * 1.5;
-	            break;
-	        case Movie.NEW_RELEASE:
-	            result += daysRented * 3;
-	            break;
-	        case Movie.CHILDRENS:
-	            result += 1.5;
-	            if (daysRented > 3)
-	                result += (daysRented - 3) * 1.5;
-	            break;
-	    }
-	    return result;
-	}
 	
 	int getFrequentRenterPoints(int daysRented) {
 		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) 
